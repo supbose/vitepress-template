2982602065b4505f73ad28719d2b61289bff331b
supbose <yshxw@qq.com>
2023-03-22 13:04:46 +0800

    vitepress-theme-demoblock
    vitepress-theme-demoblock 添加完成

--- packages/docs/docs/.vitepress/theme/index.ts
+++ packages/docs/docs/.vitepress/theme/index.ts
@@ -2,16 +2,23 @@ import Theme from 'vitepress/theme'
 import { h } from 'vue'
 import AnimateTitle from './components/AnimationTitle.vue'
 import imagesArticle from './components/imagesArticle.vue'
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
+import { useComponents } from './useComponents'
+import './styles/index.css'
+import Button from '../../../src/components/Button.vue'
+import '../../../src/styles/index.css'
 
 export default {
-	...Theme,
-	enhanceApp({ app }) {
-		app.component('imagesArticle', imagesArticle)
-		// app.component('ArcoVue', ArcoVue)
-	},
-	Layout() {
-		return h(Theme.Layout, null, {
-			'home-hero-info': () => h(AnimateTitle)
-		})
-	}
+  ...Theme,
+  enhanceApp({ app }) {
+    app.component('imagesArticle', imagesArticle)
+    useComponents(app)
+    app.component(Button.name, Button)
+    // app.component('ArcoVue', ArcoVue)
+  },
+  Layout() {
+    return h(Theme.Layout, null, {
+      'home-hero-info': () => h(AnimateTitle),
+    })
+  },
 }
