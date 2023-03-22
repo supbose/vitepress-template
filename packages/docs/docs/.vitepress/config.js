17957faa576e113523115161f51690e5369f7ed6
supbose <yshxw@qq.com>
2023-03-22 18:38:51 +0800

    增加几个示例

--- packages/docs/docs/.vitepress/config.js
+++ packages/docs/docs/.vitepress/config.js
@@ -2,7 +2,7 @@
  * @ Author: supbose
  * @ Create Time: 2023-03-15 10:28:51
  * @ Modified by: supbose
- * @ Modified time: 2023-03-22 13:00:33
+ * @ Modified time: 2023-03-22 13:14:20
  * @ Description: qingshu.work
  */
 
@@ -73,13 +73,12 @@ export default defineConfig({
     // 	pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
     // 	text: '编辑'
     // },
-
     socialLinks: [
       {
         icon: {
           svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>码云</title><path d="M11.984 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.016 0zm6.09 5.333c.328 0 .593.266.592.593v1.482a.594.594 0 0 1-.593.592H9.777c-.982 0-1.778.796-1.778 1.778v5.63c0 .327.266.592.593.592h5.63c.982 0 1.778-.796 1.778-1.778v-.296a.593.593 0 0 0-.592-.593h-4.15a.592.592 0 0 1-.592-.592v-1.482a.593.593 0 0 1 .593-.592h6.815c.327 0 .593.265.593.592v3.408a4 4 0 0 1-4 4H5.926a.593.593 0 0 1-.593-.593V9.778a4.444 4.444 0 0 1 4.445-4.444h8.296Z"/></svg>',
         },
-        link: 'https://git.qingshu.work/qingshu/docs.git',
+        link: 'https://git.qingshu.work/supbose/template.git',
       },
     ],
 
